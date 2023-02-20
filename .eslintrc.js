369154939606317a541257ee3ba41b36df4739dd
lcjltj <lcjltj@gmail.com>
2023-02-20 13:12:57 +0900

    refactor: eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,59 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				endOfLine: 'auto',
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				'eslint.validate': [
+					{
+						language: 'vue',
+						autoFix: true,
+					},
+					{
+						language: 'javascript',
+						autoFix: true,
+					},
+					{
+						language: 'javascriptreact',
+						autoFix: true,
+					},
+					{
+						language: 'typescript',
+						autoFix: true,
+					},
+					{
+						language: 'typescriptreact',
+						autoFix: true,
+					},
+				],
+				'editor.codeActionsOnSave': {
+					'source.fixAll.eslint': true,
+				},
+				// don't format on save
+				'editor.formatOnSave': false,
+			},
+		],
+	},
 };
